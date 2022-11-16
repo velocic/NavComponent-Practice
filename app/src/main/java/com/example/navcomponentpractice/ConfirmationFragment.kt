c91514348810515aa7df4d52c6c03ae8615bc166
velocic <velocicdev@gmail.com>
2022-11-16 16:17:40 -0500

    Add missing Hilt annotation to confirmation fragment

--- app/src/main/java/com/example/navcomponentpractice/ConfirmationFragment.kt
+++ app/src/main/java/com/example/navcomponentpractice/ConfirmationFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.navcomponentpractice.databinding.FragmentConfirmationBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ConfirmationFragment : Fragment() {
 
     private val viewModel: ConfirmationViewModel by viewModels()
